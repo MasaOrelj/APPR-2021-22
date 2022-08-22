b93bcd4355f7800cd899c03b5fbda292e06289a8
Maša Orelj <you@example.com>
2022-08-22 13:20:29 +0200

    Popravljeni podatki, narejeni grafi in uvoz

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -15,8 +15,8 @@ sl <- locale("sl", decimal_mark=",", grouping_mark=".")
 
 
   
-tabela_izobrazba <- read_csv(
-  "podatki/izobrazba.csv",
+tabela_izobrazba <- read_csv2(
+  "podatki/izobrazba_EU.csv",
   skip = 1, na=c(":"),
   locale = locale(encoding = "Windows-1250"),
   col_types = cols(
@@ -27,40 +27,32 @@ tabela_izobrazba <- read_csv(
 tabela_izobrazba <- tabela_izobrazba %>% 
   mutate(
     Stolpec =
-      str_replace_all(Stolpec, "y,", "y-")
+      str_replace_all(Stolpec, "primary,", "primary-")
   ) %>%
-  separate(col = Stolpec, into = c("Leto","Država", "Spol", "Starost", "Odstotek", "Stopnja izobrazbe","Vrednost"), sep = ",", convert = FALSE) %>%
+  separate(col = Stolpec, into = c("Leto","Drzava", "Spol", "Starost", "Odstotek", "Stopnja.izobrazbe","Vrednost"), sep = ",", convert = FALSE) %>%
   select(-"Starost", -"Odstotek") %>%
   mutate_all(str_replace_all, '"', '') %>%
   mutate(Vrednost = parse_number(Vrednost), Leto = parse_number(Leto))
  
 
-#lapply(tabela_izobrazba, gsub, pattern = "y,", replacement = "y-")
-#tabela_izobrazba <- as.data.frame(tabela_izobrazba_popravek) 
-#tabela_izobrazba <- separate(data = tabela_izobrazba, col = Stolpec, into = c("Leto","Država", "Spol", "Starost", "Odstotek", "Stopnja izobrazbe","Vrednost"), sep = ",", convert = TRUE) %>% select(-"Starost", -"Odstotek") 
-#tabela_izobrazba <- lapply(tabela_izobrazba, gsub, pattern = "y-", replacement = "y,")
-#tabela_izobrazba <- as.data.frame(tabela_izobrazba) 
-#tabela_izobrazba <- tabela_izobrazba %>% mutate_all(str_replace_all, '"', '')
-
-
     
 
 tabela_sodelovanje <- read_csv(
-  "podatki/sodelovanje_v_turizmu.csv",
+  "podatki/sodelovanje_v_tur.csv",
   skip = 1, na=c(":"),
   locale = locale(encoding = "Windows-1250"),
   col_types = cols(
     .default = col_guess()),
-  col_names = c("Leto", "Država", "Enota", "Trajanje", "Lokacija", "Spol", "Število sodelujočih")) %>% select(-"Trajanje", -8)
+  col_names = c("Leto", "Drzava", "Enota", "Trajanje", "Lokacija", "Spol", "Stevilo.sodelujocih")) %>% select(-"Trajanje", -8)
 
 
 tabela_stevilo_potovanj <- read_csv(
-  "podatki/stevilo_potovanj.csv",
+  "podatki/st_potovanj.csv",
   skip = 1, na=c(":"),
   locale = locale(encoding = "Windows-1250"),
   col_types = cols(
     .default = col_guess()),
-  col_names = c("Leto", "Država", "Enota", "Namen", "Trajanje", "Lokacija", "Spol", "Število potovanj")) %>% select(-"Enota", -"Trajanje", -9)
+  col_names = c("Leto", "Drzava", "Enota", "Namen", "Trajanje", "Lokacija", "Stevilo.potovanj")) %>% select(-"Enota", -"Trajanje", -8)
 
 tabela_potrosnja <- read_csv(
   "podatki/potrosnja.csv",
@@ -68,56 +60,174 @@ tabela_potrosnja <- read_csv(
   locale = locale(encoding = "Windows-1250"),
   col_types = cols(
     .default = col_guess()),
-  col_names = c("Leto", "Država", "Enota", "Namen", "Trajanje", "Lokacija", "Nastanitev", "Nočitev/Potovanje", "Povprečna potrošnja")) %>% select(-"Enota", -"Trajanje", -"Nastanitev")
-
-
+  col_names = c("Leto", "Drzava", "Enota", "Namen", "Trajanje", "Lokacija", "Nastanitev", "Nocitev/Potovanje", "Povprecna.potrosnja")) %>% select(-"Enota", -"Trajanje", -"Nastanitev")
 
 
 link <- "https://en.wikipedia.org/wiki/List_of_European_countries_by_average_wage"
 stran <- html_session(link) %>% read_html()
-tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable mW-datatable']") %>%
-  .[[2]] %>% html_table(dec=",")
+place_evropa <- stran %>% 
+  html_nodes(xpath="//table[@class='wikitable sortable']") %>%
+  .[[3]] %>% 
+  html_table(fill=TRUE)
 
-tabela <- tabela[-1,c(1,2,7)] %>% dplyr::rename(TIME = Year) %>% dplyr::rename(GEO = Country)
+place_evropa <- place_evropa[-1:-2,-2:-15] 
+place_evropa <- place_evropa[,-9]
 
-tabela$TIME <- as.numeric(tabela$TIME)
+izbris <- c("United Kingdom",
+                      "Serbia",
+                      "North Macedonia",
+                      "Montenegro",
+                      "Kosovo",
+                      "Georgia",
+                      "Bosnia and Herzegovina",
+                      "Armenia",
+                      "Albania",
+                      "Ukraine",
+                      "Iceland",
+                      "Moldova",
+                      "Norway"
+)
+
+colnames(place_evropa) <- c("Drzava", 2012:2018)
+
+place_evropa <- place_evropa[ ! place_evropa$Drzava %in% izbris, ]
 
 
-uvozi.povp_placa <- function() {
   
-  link <- "https://en.wikipedia.org/wiki/List_of_European_countries_by_average_wage"
-  page <- html_session(link) %>% read_html()
-  table <- page %>% html_nodes(xpath="//table[@class='wikitable nowrap sortable mw-datatable']") %>% .[[1]] %>%
-    html_table(dec=".")
+place_evropa <- place_evropa %>% 
+  mutate(Drzava=gsub("\\[[^]]*\\]","", Drzava)) %>%
+  pivot_longer(-c(1), names_to = "Leto", values_to = "Placa") %>%
+  mutate(Placa=parse_number(Placa, na=c("-", "NA", "")), Leto=parse_number(Leto)) %>% arrange(Leto) 
+place_evropa <- place_evropa[c(2,1,3)]
+
+
+#združevanje tabel
+
+potrosnja_stolpec <- tabela_potrosnja[,6]
+potrosnja_razdeljeni_stolpec1 <- potrosnja_stolpec[seq(1, nrow(potrosnja_stolpec),2),]
+
+potrosnja_razdeljeni_stolpec2 <- potrosnja_stolpec[seq(2, nrow(potrosnja_stolpec),2),]
+
+tabela_potrosnja2 <- tabela_potrosnja[,-5:-6]
+tabela_potrosnja2 <- tabela_potrosnja2[seq(1, nrow(tabela_potrosnja2),2),]
+tabela_potrosnja2 <- cbind(tabela_potrosnja2, potrosnja_razdeljeni_stolpec1, potrosnja_razdeljeni_stolpec2)
+colnames(tabela_potrosnja2)[5:6] <- c("Izlet", "Nocitev")
+
+
+
+tabela_potrosnja_izleti <- tabela_stevilo_potovanj %>%
+  left_join(tabela_potrosnja2, by = c("Drzava", "Leto", "Namen", "Lokacija"))
+
+
+colnames(tabela_potrosnja_izleti)[5] <- c(" ")
+tabela_potrosnja_izleti <- tabela_potrosnja_izleti %>% mutate("Skupaj" =  tabela_potrosnja_izleti[,5] * tabela_potrosnja_izleti[,6] ) 
+
+
+stolpec_potrosnja <- tabela_potrosnja_izleti[["Skupaj"]]
+
+tabela_potrosnja_izleti <- tabela_potrosnja_izleti %>% select(-"Skupaj")
+tabela_potrosnja_izleti <- cbind(tabela_potrosnja_izleti, stolpec_potrosnja)
+
+colnames(tabela_potrosnja_izleti) <- c("Leto", "Drzava", "Namen", "Lokacija", "Stevilo.potovanj", "Povprecna.potrosnja.na.izlet", "Povprecna.potrosnja.na.noc", "Skupna.potrosnja")
+
+tabela_potrosnja_izleti$Drzava[tabela_potrosnja_izleti$Drzava == "Germany (until 1990 former territory of the FRG)"] <- "Germany"
+
+
+Potovanja.Skupaj <- tabela_potrosnja_izleti %>% 
+  group_by(Leto) %>%
+  summarise(Stevilo.potovanj=sum(Stevilo.potovanj[!is.na(Stevilo.potovanj)]))
+
+Potrosnja.Skupaj <- tabela_potrosnja_izleti %>% 
+  group_by(Leto) %>%
+  summarise(Skupna.potrosnja=sum(Skupna.potrosnja[!is.na(Skupna.potrosnja)]))
   
-  
-  #for (i in 1:ncol(table())) {
-  # if (is.character(table[[i]])) {
-  #   Encoding(table[[i]]) <- "UTF-8"
-  # }
-  #}
-  
-  colnames(table) <- c("Countries", "Total alcohol", "Recorded consumption", "Unrecorded consumption", "Beer(%)",
-                       "Wine(%)", "Spirits(%)", "Other(%)", "2015 projection")
-  
-  table$`2015 projection`<- NULL
-  table$`Recorded consumption` <- NULL
-  table$`Unrecorded consumption`<- NULL
-  
-  # for (col in c("Beer", "Wine", "Spirits", "Other", "Total alcohol")) {
-  #   if (is.character(table[[col]])) {
-  #     table[[col]] <- parse_number(table[[col]], na=c("0", "0.0", ""))
-  #   }
-  # }
-  return(table)
-}
-kolicina_pc <- uvozi.povp_placa()
+
+Potrosnja.Skupaj$Skupna.potrosnja <- Potrosnja.Skupaj$Skupna.potrosnja/1000000
+
+#druga tabela - izobrazba, sodelovanje
+
+izobrazba_stolpec <- tabela_izobrazba[,5]
+izobrazba_stolpec1 <- izobrazba_stolpec[seq(1, nrow(izobrazba_stolpec),3),]
+izobrazba_stolpec2 <- izobrazba_stolpec[seq(2, nrow(izobrazba_stolpec),3),]
+izobrazba_stolpec3 <- izobrazba_stolpec[seq(3, nrow(izobrazba_stolpec),3),]
+
+tabela_izobrazba2 <- tabela_izobrazba %>% select(-"Stopnja.izobrazbe", -"Vrednost") %>% filter(row_number() %% 3 == 1)
+tabela_izobrazba2 <- cbind(tabela_izobrazba2, izobrazba_stolpec1, izobrazba_stolpec2, izobrazba_stolpec3)
+colnames(tabela_izobrazba2)[4:6] <- c("Less.than.primary.primary.and.lower.secondary.education", "Upper.secondary.and.post-secondary.non-tertiary.education", "Tertiary.education")
+
+
+tabela_sodelovanje2 <- tabela_sodelovanje[!grepl("Number", tabela_sodelovanje$Enota),]
+tabela_sodelovanje2 <- tabela_sodelovanje2 %>% select(-"Enota")
+colnames(tabela_sodelovanje2)[5] <- "Odstotek.sodelujocih"
+
+tabela_domestic <- tabela_sodelovanje2[!grepl("All.countries.of.the.world", tabela_sodelovanje2$Lokacija),]
+tabela_domestic <- tabela_domestic %>% select(-"Lokacija")
+
+tabela_all_countries <- tabela_sodelovanje2[!grepl("Domestic", tabela_sodelovanje2$Lokacija),]
+tabela_all_countries <- tabela_all_countries %>% select(-"Lokacija")
+
+tabela_izobrazba_sodelovanje <- tabela_domestic %>%
+  left_join(tabela_all_countries, by = c("Drzava", "Leto", "Spol"))
+
+colnames(tabela_izobrazba_sodelovanje)[4:5] = c("Odstotek.sodelujocih.znotraj.drzave", "Odstotek.vseh.sodelujocih")
+
+tabela_izobrazba_sodelovanje$'Odstotek.sodelujocih.izven.drzave' <- (tabela_izobrazba_sodelovanje$`Odstotek.vseh.sodelujocih` - tabela_izobrazba_sodelovanje$`Odstotek.sodelujocih.znotraj.drzave`)
+
+tabela_izobrazba_sodelovanje <- tabela_izobrazba_sodelovanje %>% .[c(1,2,3,4,6,5)] %>%
+  left_join(tabela_izobrazba2, by = c("Drzava", "Leto", "Spol"))
+
+tabela_izobrazba_sodelovanje$Drzava[tabela_izobrazba_sodelovanje$Drzava == "Germany (until 1990 former territory of the FRG)"] <- "Germany"
+
+
+#povprečna plača in potrošnja
+
+place_evropa$Drzava[place_evropa$Drzava == "Czech Republic"] <- "Czechia"
+place_evropa <- place_evropa %>%  mutate(Drzava=gsub(" ","", Drzava))
+
+pomozna <- tibble(Drzava = c(
+  "Austria", "Belgium", "Bulgaria", "Croatia", "Cyprus", "Czechia", "Denmark", "Estonia",
+  "Finland", "Germany", "Hungary", "Ireland", "Lithuania", "Latvia", "Luxembourg", "Poland", "Portugal", "Romania",
+  "Slovakia", "Slovenia", "Spain", "Sweden")
+)
+
+pomozna_potrosnja_1 <- tabela_potrosnja_izleti %>% select(c("Leto", "Drzava", "Namen", "Lokacija","Povprecna.potrosnja.na.izlet", "Povprecna.potrosnja.na.noc")) %>%
+  filter(Namen == "Personal reasons")
+
+pomozna_potrosnja_2 <- tabela_potrosnja_izleti %>% select(c("Leto", "Drzava","Namen", "Lokacija", "Povprecna.potrosnja.na.izlet", "Povprecna.potrosnja.na.noc")) %>%
+  filter(Namen == "Professional, business")
+
+tabela_potrosnja_1 <- pomozna_potrosnja_1 %>% select("Leto", "Drzava", "Lokacija")
+tabela_potrosnja_1$Potrosnja.izlet <- ((pomozna_potrosnja_1$Povprecna.potrosnja.na.izlet + pomozna_potrosnja_2$Povprecna.potrosnja.na.izlet)/2)
+tabela_potrosnja_1$Potorsnja.noc <- ((pomozna_potrosnja_1$Povprecna.potrosnja.na.noc + pomozna_potrosnja_2$Povprecna.potrosnja.na.noc)/2)
+
+pomozna_potrosnja_3 <- tabela_potrosnja_1 %>% filter(Lokacija == "Domestic country")
+pomozna_potrosnja_4 <- tabela_potrosnja_1 %>% filter(Lokacija == "Outbound")
+tabela_potrosnja_1 <- pomozna_potrosnja_3 %>% select("Leto","Drzava") 
+tabela_potrosnja_1$Potrosnja.na.izlet <- ((pomozna_potrosnja_3$Potrosnja.izlet + pomozna_potrosnja_4$Potrosnja.izlet)/2) 
+tabela_potrosnja_1$Potrosnja.na.noc <- ((pomozna_potrosnja_3$Potorsnja.noc + pomozna_potrosnja_4$Potorsnja.noc)/2)
+
+tabela_potrosnja_1 <- tabela_potrosnja_1[tabela_potrosnja_1$Drzava %in% 
+                                           c("Austria", "Belgium", "Latvia", "Bulgaria", "Croatia", "Cyprus",
+                                             "Czechia", "Denmark", "Estonia",
+                                            "Finland", "Germany", "Hungary", "Ireland", "Lithuania", "Luxembourg",
+                                             "Poland", "Portugal", "Romania","Slovakia", "Slovenia", "Spain", "Sweden"),]  
+
+tabela_potrosnja_1 <- tabela_potrosnja_1[!grepl("2019", tabela_potrosnja_1$Leto),]
+tabela_potrosnja_1 <- tabela_potrosnja_1 %>% arrange(Drzava) 
+
+
+tabela_place_potrosnja <- pomozna %>% 
+  left_join(place_evropa, by = "Drzava")
+tabela_place_potrosnja[with(tabela_place_potrosnja, order(Leto, Drzava)), ]
+
+
+
+tabela_place_potrosnja <- tabela_place_potrosnja %>%
+  left_join(tabela_potrosnja_1, by = c("Drzava", "Leto")) 
 
 
 
 
 
-
-
-
+# Glavne tri tabele: tabela_izobrazba_sodelovanje, tabela_potrosnja_izleti, tabela_place_potrosnja, dve pomembni manjsi tabeli sta Potovanja.Skupaj in Potrosnja.Skupaj
 
--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,1 +1,231 @@
 # 3. faza: Vizualizacija podatkov
+
+library(ggplot2)
+
+library(readr)
+library(dplyr)
+library(tidyr)
+library(tidyverse)
+library(plyr)
+library(tibble)
+library(stringr)
+library(xml2)
+library(rvest)
+library(plotly)
+
+#Sodelovanje v turizmu med visoko izobraženimi državami glede na spol
+
+Visoka.izobrazba <- tabela_izobrazba_sodelovanje %>%
+  filter(Spol == "Total") %>%
+  group_by(Drzava) %>%
+  summarise(Povprecna.visoka.izobrazba=mean(Tertiary.education[!is.na(Tertiary.education)])) %>% arrange(Povprecna.visoka.izobrazba)
+Drzave1 <- Visoka.izobrazba[21:27,1]
+Drzave1 <- Drzave1[!grepl("Luxembourg", Drzave1$Drzava),]
+
+Drzave2 <- Visoka.izobrazba[1:6,1]
+
+tabela_izobrazba_sodelovanje <-na.omit(tabela_izobrazba_sodelovanje)
+         
+graf1 <- tabela_izobrazba_sodelovanje %>% 
+  filter(Drzava %in% Drzave1$Drzava) %>%
+  
+  ggplot(
+    mapping = aes(x = Leto, y = Odstotek.vseh.sodelujocih , color = Spol)
+  ) +
+  labs(
+    x = "Leto",
+    y = "Odstotek",
+    title = "Sodelovanje v turizmu med visoko izobraženimi državami glede na spol"
+  ) +
+  theme_classic() +
+  theme(
+    axis.text.x = element_text(angle = 45, vjust = 0.5),
+    axis.title.x = element_text(vjust = 0)
+  )+
+  geom_line(aes(linetype = Spol)) +
+  geom_point()+
+  scale_linetype_manual(values=c("solid", "solid","twodash")) +
+  
+  facet_wrap(. ~ Drzava, ncol = 3) +
+  coord_cartesian(ylim = c(20,100))
+
+graf1
+
+#Sodelovanje v turizmu med nizko izobraženimi državami glede na spol
+
+graf2 <- tabela_izobrazba_sodelovanje %>%
+  filter(Drzava %in% Drzave2$Drzava) %>%
+  
+  ggplot(
+    mapping = aes(x = Leto, y = Odstotek.vseh.sodelujocih , color = Spol)
+  ) +
+  labs(
+    x = "Leto",
+    y = "Odstotek",
+    title = "Sodelovanje v turizmu med nizko izobraženimi državami glede na spol"
+  ) +
+  theme_classic() +
+  theme(
+    axis.text.x = element_text(angle = 45, vjust = 0.5),
+    axis.title.x = element_text(vjust = 0)
+  )+
+  geom_line(aes(linetype = Spol)) +
+  geom_point()+
+  scale_linetype_manual(values=c("solid", "solid","twodash")) +
+  
+  facet_wrap(. ~ Drzava, ncol = 3) +
+  coord_cartesian(ylim = c(20, 100))
+
+graf2
+
+#Dokaz, da spol ni pomemben faktor v sodelovanju v turizmu
+
+graf3 <- tabela_izobrazba_sodelovanje %>%
+  ggplot(
+    mapping = aes(x = Spol, y = Odstotek.vseh.sodelujocih)
+  ) +
+  geom_boxplot() +
+  labs(
+    x = "Spol",
+    y = "Odstotek vseh sodelujočih",
+    title = "Povezava med sodelovanjem v turizmu in spolom v državah EU"
+  )
+ggplotly(graf3)
+
+
+#Sodelovanje v turizmu med visoko izobraženimi državami glede na lokacijo
+
+graf4 <- tabela_izobrazba_sodelovanje %>% filter(Spol == "Total", Drzava %in% Drzave1$Drzava) %>%
+  ggplot(
+    mapping= aes(x = Leto)
+  ) +
+  geom_line(aes(y = Odstotek.sodelujocih.znotraj.drzave), color="Blue", size = 1) +
+  geom_line(aes(y = Odstotek.sodelujocih.izven.drzave), color="Green", size = 1) +
+  geom_line(aes(y= Odstotek.vseh.sodelujocih), color="red") +
+  facet_wrap(. ~ Drzava, ncol = 3) +
+  theme_classic() +
+    theme(
+      axis.text.x = element_text(angle = 45, vjust = 0.5),
+      axis.title.x = element_text(vjust = 0)
+    )+
+  labs(
+    x = "Leto",
+    y = "Odstotek",
+    title = "Sodelovanje v turizmu med visoko izobraženimi državami glede na lokacijo") 
+  
+  
+ggplotly(graf4)
+
+#Sodelovanje v turizmu med nizko izobraženimi državami glede na lokacijo
+colors1 <- c("Znotraj države" = "blue", "Izven države" = "green", "Skupaj" = "red")
+graf5 <- tabela_izobrazba_sodelovanje %>% filter(Spol == "Total", Drzava %in% Drzave2$Drzava) %>%
+  ggplot(
+    mapping= aes(x = Leto)
+  ) +
+  geom_line(aes(y = Odstotek.sodelujocih.znotraj.drzave, color="Znotraj države"), size = 1) +
+  geom_line(aes(y = Odstotek.sodelujocih.izven.drzave, color="Izven države"), size = 1) +
+  geom_line(aes(y= Odstotek.vseh.sodelujocih, color="Skupaj")) +
+  facet_wrap(. ~ Drzava, ncol = 3) +
+  labs(
+    x = "Leto",
+    y = "Odstotek",
+    title = "Sodelovanje v turizmu med nizko izobraženimi državami glede na lokacijo",
+    color = "Legend"
+  ) +
+  theme_classic() +
+  theme(
+    axis.text.x = element_text(angle = 45, vjust = 0.5),
+    axis.title.x = element_text(vjust = 0)
+  )+
+  scale_color_manual(values = colors1)
+
+ggplotly(graf5)
+
+
+
+#Skupna potrošnja v državah EU od 2012 do 2019
+
+graf6 <- Potrosnja.Skupaj%>%
+  ggplot(mapping=aes(x=Leto, y=Skupna.potrosnja)) + geom_bar(stat= "identity", color="blue", fill="light blue") +
+  labs(
+    x = "Leto",
+    y = "Skupna potrošnja v milijonih",
+    title = "Skupna potrošnja držav EU"
+  ) +
+  coord_cartesian(ylim = c(75000, 175000)) +
+  stat_summary(geom ="line",
+               fun = mean,
+               size = 1)
+ggplotly(graf6)
+
+#Skupno število potovanj v državah EU od leta 2012 do 2019
+graf7 = Potovanja.Skupaj %>%
+  ggplot(
+    mapping = aes(x = Leto, y = Stevilo.potovanj)
+  ) +
+  labs(
+    x = "Leto",
+    y = "Skupno število potovanj",
+    title = "Skupno število potovanj držav EU"
+  ) +
+  theme_light() +
+  theme(
+    axis.text.x = element_text(angle = 45, vjust = 0.5),
+    axis.title.x = element_text(vjust = 0)
+  )+
+  geom_line(color='light blue', arrow = arrow(angle = 20), size=2)+
+  geom_point(size = 2, color='blue')
+
+graf7
+
+#Povezava med povprečno neto plačo in potrošnjo v turizmu v EU
+tabela_place_potrosnja <- na.omit(tabela_place_potrosnja)
+colors <- c("Potrosnja.na.izlet" = "blue", "Potrosnja.na.noc" = "green")
+graf8 <- tabela_place_potrosnja %>%
+  ggplot(mapping= aes(x = Placa),
+    geom = "point",
+    stat = "identity",
+    position = "identity"
+  ) +
+  geom_point(aes(y= Potrosnja.na.izlet, color = "Potrosnja.na.izlet")) +
+  geom_smooth(aes(y =Potrosnja.na.izlet), method=lm)+
+  geom_point(aes(y=Potrosnja.na.noc, color = "Potrosnja.na.noc"))+
+  geom_smooth(aes(y=Potrosnja.na.noc), method=lm, color = "green")+
+  labs(
+    x = "Povprečna neto plača",
+    y = "Povprečna potrošnja",
+    title = "Povezava med povprečno neto plačo in povprečno potrošnjo na izlet in na noč v turizmu",
+    color = "Legend") +
+  scale_color_manual(values = colors)
+    
+  ggplotly(graf8)
+
+
+#Povezava med stopnjo visoko izobraženih in sodelujočih v izvendržavnem turizmu
+visokoizobrazeni_sodelovanje <- tabela_izobrazba_sodelovanje %>%
+  filter(Spol == "Total")
+visokoizobrazeni_sodelovanje <- na.omit(visokoizobrazeni_sodelovanje)
+
+graf9 <- visokoizobrazeni_sodelovanje %>%
+  ggplot(
+    mapping = aes(x = Odstotek.sodelujocih.izven.drzave, y = Tertiary.education, color = Leto),
+    geom = "point",
+    stat = "identity",
+    position = "identity"
+  ) +
+  labs(
+    x = "Odstotek sodelujočih v turizmu izven države",
+    y = "Odstotek visokoizobraženega prebivalstva",
+    title = "Povezava med visoko izobrazbo in sodelovanjem v turizmu izven države"
+  ) +
+  geom_point(size = 3)+
+  geom_smooth() 
+graf9
+
+#Zemljevid odstotka sodelujočih v turizmu v državah EU leta 2020
+
+
+
+
+
+
