aa6c8730cad3823bade6d3242feed7bb5854299d
Maša Orelj <you@example.com>
2022-08-29 17:25:47 +0200

    slovnični popravki

--- shiny/ui.R
+++ shiny/ui.R
@@ -3,7 +3,7 @@ library(shiny)
 fluidPage(
   titlePanel("Spreminjanje števila potovanj v državah EU"),
   column(3,
-         selectInput("select", label = h3("Izberi državo"), 
+         selectInput("select", label = "Izberi državo", 
                      choices = list("Slovenia", "Austria", "Spain", "France", "Germany", "Luxembourg", "Denmark", "Belgium",
                                     "Bulgaria", "Czechia", "Estonia", "Ireland", "Greece", "Croatia", "Cyprus", "Italy", "Latvia",
                                     "Lithuania", "Malta", "Hungary", "Netherlands", "Poland", "Portugal", "Romania", "Slovakia", "Finland",
